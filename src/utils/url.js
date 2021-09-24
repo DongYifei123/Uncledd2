64bc3a8a23df22dcef7a494e4ac891f6790f1da0
DongYifei123 <847075182@qq.com>
2021-09-24 11:06:41 +0800

    新增公众号二维码

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,1 +1,7 @@
-const host = 'localhost';
+//  主机暂时使用package.json中的代理功能，只需要对应接口即可
+
+// 调用照片接口，一次返回20张照片信息，参数为photoId，返回id的后20张照片，photoId=-2时表示第一次加载，
+export const Show20Photos = '/Show20Photos'
+
+// 下载照片接口，参数为photoId
+export const DownloadPhoto = '/DownloadPhoto'
